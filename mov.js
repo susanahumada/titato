ccf61259af2396f95a6518deb90518bc2ebcb878
susanahumada <33757293+susanahumada@users.noreply.github.com>
2017-11-23 19:54:00 -0700

    Update mov.js

--- mov.js
+++ mov.js
@@ -82,6 +82,33 @@ function asd() {
   })
 }
 
+function asd() {
+    console.log(String(cacheKeys.innerHTML))
+    firebase.database().ref('game/').set({
+        progress: String(cacheKeys),
+        turn: 'X'
+  })
+}
+
+function asd2() {
+    firebase.database().ref('game/').set({
+        progress: `
+        <div>
+             <button id="b11" class="buttons" name="b1" onclick="actions(this)"></button>
+             <button id="b12" class="buttons" name="b2" onclick="actions(this)"></button>
+             <button id="b13" class="buttons" name="b3" onclick="actions(this)"></button>
+             <button id="b21" class="buttons" name="b4" onclick="actions(this)"></button>
+             <button id="b22" class="buttons" name="b5" onclick="actions(this)"></button>
+             <button id="b23" class="buttons" name="b6" onclick="actions(this)"></button>
+             <button id="b32" class="buttons" name="b8" onclick="actions(this)"></button>
+             <button id="b31" class="buttons" name="b7" onclick="actions(this)"></button>
+             <button id="b33" class="buttons" name="b9" onclick="actions(this)"></button>
+         </div>
+         `,
+        turn: 'X'
+  })
+}
+
 var starCountRef = firebase.database().ref('game/');
 starCountRef.on('value', function(snapshot) {
     let el = snapshot.val()
